71eb46d20242a0146cab1edaa8ec08cd54b76f07
Himanshu gaur <himanshugaur055@gmail.com>
2025-02-22 15:25:22 +0530

    stats

--- components/dashboard/blog-dashboard.tsx
+++ components/dashboard/blog-dashboard.tsx
@@ -1,14 +1,62 @@
+import Link from "next/link";
 import React from "react";
+import { Button } from "../ui/button";
+import { FileText, MessageCircle, PlusCircle } from "lucide-react";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 
 const BlogDashboard = () => {
   return (
-    <main className="flex-1 p-4 md:p-8">
+    <main className="flex-1  md:p-8">
       <div className="flex justify-between items-center mb-8 ">
         <div>
-        <h1 className="font-bold text-2xl">Blog Dashboard</h1>
-        <p >Manage your content and analytics</p>
+          <h1 className="font-bold text-2xl">Blog Dashboard</h1>
+          <p>Manage your content and analytics</p>
         </div>
-       
+        <Link href="/dashboard/articles/create">
+          <Button>
+            <PlusCircle className="w-4 h-4 " />
+            New Article
+          </Button>
+        </Link>
+      </div>
+
+      <div className="grid md:grid-cols-3 mb-8 gap-4">
+        <Card>
+          <CardHeader className="flex flex-row  items-center justify-between space-y-0 pb-2">
+            <CardTitle className="font-medium text-sm">
+              Total Articles
+            </CardTitle>
+            <FileText className="w-4 h-4 " />
+          </CardHeader>
+          <CardContent >
+            <div className=" text-2xl font-bold ">2</div>
+            <p className="text-sm text-muted-foreground mt-1">+5 from last month</p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row  items-center justify-between space-y-0 pb-2">
+            <CardTitle className="font-medium text-sm">
+              Total Comments
+            </CardTitle>
+            <MessageCircle className="w-4 h-4 " />
+          </CardHeader>
+          <CardContent>
+            <div className=" text-2xl font-bold ">2</div>
+            <p className="text-sm text-muted-foreground mt-1">12 waiting</p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row  items-center justify-between space-y-0 pb-2">
+            <CardTitle className="font-medium text-sm">
+              Avg. Rating Time
+            </CardTitle>
+            <MessageCircle className="w-4 h-4 " />
+          </CardHeader>
+          <CardContent>
+            <div className=" text-2xl font-bold ">4.2</div>
+            <p className="text-sm text-muted-foreground mt-1">+0.6 from last month</p>
+          </CardContent>
+        </Card>
       </div>
     </main>
   );
