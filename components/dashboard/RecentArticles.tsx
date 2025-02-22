2c4b5f7b32e40ce11f9d552d8ec01eb9c3e40add
Himanshu gaur <himanshugaur055@gmail.com>
2025-02-22 20:53:03 +0530

    table added for better insights

--- components/dashboard/RecentArticles.tsx
+++ components/dashboard/RecentArticles.tsx
@@ -1,6 +1,16 @@
 import React from "react";
-import { Card, CardHeader, CardTitle } from "../ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { Button } from "../ui/button";
+
+import {
+    Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+import { Badge } from "../ui/badge";
 
 const RecentArticles = () => {
   return (
@@ -17,6 +27,28 @@ const RecentArticles = () => {
           </Button>
         </div>
       </CardHeader>
+      <CardContent>
+    <Table>
+             <TableHeader>
+            <TableRow>
+              <TableHead>Title</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Comments</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead>Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>
+                <Badge variant={'secondary'} className="rounded-full bg-green-100 text-green-800">
+                  Published
+                </Badge>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+          </Table> 
+      </CardContent>
     </Card>
   );
 };
