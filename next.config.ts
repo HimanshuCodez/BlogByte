f6efd862a80c6de8340bd65fee38f499f6525091
Himanshu gaur <himanshugaur055@gmail.com>
2025-02-25 20:41:52 +0530

    folder routing fixed

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,10 @@ const nextConfig: NextConfig = {
       protocol: 'https',
       hostname: 'in.pinterest.com',
     },
+    {
+      protocol: 'https',
+      hostname: 'res.cloudinary.com',
+    },
     ]
   }
 };
