f4f2ed6f63705d14af8a2eb635d56a8303a17c86
Himanshu gaur <himanshugaur055@gmail.com>
2025-03-02 10:41:32 +0530

    fixes

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="min-h-screen bg-background">
+      <main className="container  mx-auto px-4 py-12 sm:px-6 lg:text-5xl">
+        <div className="mb-12 space-y-6 text-center">
+          <h1 className="text-4xl font-bold tracking-tight text-foreground sm:text-5xl">
+            All Articles
+          </h1>
+        </div>
+      </main>
+    </div>
+  );
+};
 
-export default page
+export default page;
