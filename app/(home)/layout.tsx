f63f9aeec23f24a32890070412a922e76933e4f8
Himanshu gaur <himanshugaur055@gmail.com>
2025-03-04 00:56:46 +0530

    deployment sign up fixes fcukk

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,32 +1,42 @@
+import Navbar  from "@/components/home/header/navbar";
 import { prisma } from "@/lib/prisma";
 import { SignIn } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
 import React from "react";
 
-const Layout = async ({ children }: { children: React.ReactNode }) => {
+const layout = async ({ children }: { children: React.ReactNode }) => {
+ 
   const user = await currentUser();
-
   if (!user) {
-    // Redirect to sign-in page instead of returning null
-    return <SignIn/>
+    return <div>
+   
+    {children}
+  </div>
   }
-
   const loggedInUser = await prisma.user.findUnique({
     where: { clerkUserId: user.id },
   });
-
   if (!loggedInUser) {
     await prisma.user.create({
       data: {
+        name: `${user.fullName} ${user.lastName}`,
         clerkUserId: user.id,
-        name: user.fullName as string,
         email: user.emailAddresses[0].emailAddress,
         imageUrl: user.imageUrl,
       },
     });
   }
-
-  return <div>{children}</div>;
+  return (
+    <div>
+      
+      {!user && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
+          <SignIn />
+        </div>
+      )}
+      {children}
+    </div>
+  );
 };
 
-export default Layout;
+export default layout;
