0b1b458a82cc6382e81791213f97587945a36e7a
Himanshu gaur <himanshugaur055@gmail.com>
2025-03-04 10:14:49 +0530

    minor fixes for non auth users

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -2,41 +2,39 @@ import { prisma } from "@/lib/prisma";
 import { SignIn } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
 import React from "react";
-
-const Layout = async ({ children }: { children: React.ReactNode }) => {
+const layout = async ({ children }: { children: React.ReactNode }) => {
+ 
   const user = await currentUser();
-
   if (!user) {
-    return <div>{children}</div>;
+    return <div>
+   
+    {children}
+  </div>
   }
-
-  try {
-    let loggedInUser = await prisma.user.findUnique({
-      where: { clerkUserId: user.id },
+  const loggedInUser = await prisma.user.findUnique({
+    where: { clerkUserId: user.id },
+  });
+  if (!loggedInUser) {
+    await prisma.user.create({
+      data: {
+        name: `${user.fullName} ${user.lastName}`,
+        clerkUserId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        imageUrl: user.imageUrl,
+      },
     });
-
-    if (!loggedInUser) {
-      loggedInUser = await prisma.user.create({
-        data: {
-          clerkUserId: user.id,
-          name: user.fullName, // Clerk provides full name, no need for lastName
-          email: user.emailAddresses[0].emailAddress,
-          imageUrl: user.imageUrl,
-        },
-      });
-    }
-  } catch (error) {
-    console.error("Error fetching or creating user in Prisma:", error);
   }
-
   return (
     <div>
-      <div className="fixed top-0 left-0 w-full">
-        <SignIn />
-      </div>
+      
+      {!user && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
+          <SignIn />
+        </div>
+      )}
       {children}
     </div>
   );
 };
 
-export default Layout;
+export default layout;
--- components/articles/ArticleDetailPage.tsx
+++ components/articles/ArticleDetailPage.tsx
@@ -48,7 +48,7 @@ export async function ArticleDetailPage({ article }: ArticleDetailPageProps) {
       <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
        
         <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-lg dark:bg-gray-800">
-          <SignIn />
+          <SignIn routing="hash" />
         </div>
       </div>
     );
